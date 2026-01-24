aa53b0b9259c2db71533920e4bd0e5f9689af675
StevePeRe <123585912+StevePeRe@users.noreply.github.com>
2026-01-24 15:26:23 +0100

    Setting up project Fashion Shop

---
+++ src/main/java/com/stevecoder/tienda_moda_API/TiendaModaApiApplication.java
@@ -0,0 +1,13 @@
+package com.stevecoder.tienda_moda_API;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TiendaModaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TiendaModaApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/stevecoder/tienda_moda_API/TiendaModaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stevecoder.tienda_moda_API;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TiendaModaApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
